9c71db4d28f55c002400c384cd4a172f7c1e7ece
Sergey <sergey_1c@mail.ru>
2024-07-12 20:12:21 +0700

    Однако писать компилятор в ручную...то еще заняте!!! Но зато очень полезно.... Что сделано? лексический,синтаксический,семантический: анализатор. Оптимизатор. и генератор кода...

--- Compiler_in_C/Compiler_in_C.c
+++ Compiler_in_C/Compiler_in_C.c
@@ -1,15 +1,43 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
+#include "lexerf.h"
+//#include "parserf.h"
+//#include "codegeneratorf.h"
 
-typedef enum{
-	INT 
-} TokenTypeLiteral;
+int main(int argc, char *argv[]){
+   if(argc != 3){
+      printf("Error: correct syntax: %s <filename.unn> <output_filename>\n", argv[0]);
+      exit(1);
+   }
+   char *output_filename = malloc(sizeof(char) * 16);
+   sprintf_s(output_filename, "%s.asm", argv[2]);
 
-int main()
-{
-	
+   FILE *file;
+   file = fopen(argv[1], "r");
+
+   if(!file){
+      printf("ERROR: File not found\n");
+      exit(1);
+   }
+   Token *tokens = lexer(file);
+
+  // Node *test = parser(tokens);
+
+   //generate_code(test, "generated.asm");
+   FILE *assembly_file = fopen("generated.asm", "r");
+   if(!assembly_file){
+      printf("ERRROR");
+      exit(1);
+   }
+   char *nasm = malloc(sizeof(char) * 64);
+   char *gcc = malloc(sizeof(char) * 64);
+   sprintf_s(nasm, "nasm -f elf64 generated.asm -o generated.o", argv[2], argv[2]);
+   sprintf_s(gcc, "gcc generated.o -o %s -lc -no-pie", argv[2], argv[2]);
+   system(nasm);
+   system(gcc);
+   printf("FINISHED\n");
 }
-
